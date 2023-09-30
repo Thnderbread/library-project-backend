27a821f45f9a2bdc2e9d2855b40b4242eaceba66
Thnderbread <david.kiiing1227@gmail.com>
2023-09-30 16:55:52 -0400

    Added vercel.json for deployment. Added frontend's new deployment URL to CORS' allowed origins.

--- config/allowedOptions.js
+++ config/allowedOptions.js
@@ -2,6 +2,9 @@ const allowedOrigins = [
     'http://10.0.0.189:3000',
     'http://localhost:3000',
     'http://localhost:8080',
+    'http://library-project-frontend.vercel.app/',
+    'http://library-project-frontend-bobbys-projects-c7bd377b.vercel.app/',
+    'http://library-project-frontend-git-main-bobbys-projects-c7bd377b.vercel.app/',
 ];
 
 const allowedMethods = ['GET', 'POST', 'DELETE']
